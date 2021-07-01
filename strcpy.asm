185db9c3ea1cbe88ad668079bad1aa80444034fd
Ana Luiza Magalhães <anaslmagalhaes@gmail.com>
2021-06-30 21:51:08 -0300

    Modified comments

--- strcpy.asm
+++ strcpy.asm
@@ -1,37 +1,41 @@
 .data
-	mensagem: .asciiz "Digite a string a ser copiada:"
-	str_origem: 
+	message: .asciiz "Enter the string to be copied: "
+	str_copied: 
 	.space 32
 	
-	str_destino: 
+	str_destiny: 
 	.space 32
 	
 .text
 	main:
 		li $v0, 4
-		la $a0, mensagem
+		la $a0, message
 		syscall
 		
 		li $v0, 8
-		la $a0, str_origem #local a ser armazenado a string lida
-		li $a1, 32 #tamanho maximo da string
+		la $a0, str_copied #address to store the input
+		li $a1, 32 #max size of the string (32 chars)
 		move $s0, $a0
 		syscall
 		
-		la $s2, str_destino #carregando endereço de destino no $s2
+		la $s2, str_destiny #copy string's address
 		
-	strCopy:
-		lb $s1, ($s0) #carregando o byte em $s1 (cada char tem 1 byte)
-		beq $s1, $zero, Exit #caracter nulo = fim da string
-		sb $s1,($s2) #salvando byte no endereço de destino
-		addi $s2, $s2, 1 #incrementando 1 byte no endereço de destino
-		addi $s0, $s0, 1 #incrementando 1 byte no endereço de origem
-		j strCopy
-		  
-	Exit: #print e fim do programa
+		
+	str_copy:
+		lb $s1, ($s0) #load a byte (char = 1 byte)
+		beq $s1, $zero, exit #end of string -> null character
+		sb $s1,($s2) #stores the char
+		#####iterator#####
+		addi $s2, $s2, 1 
+		addi $s0, $s0, 1 
+		#################
+		j str_copy
+	
+	#print and end#	  
+	exit: 
 	
 		li $v0, 4
-		la $a0, str_destino
+		la $a0, str_destiny
 		syscall
 		
 		li $v0, 10 
