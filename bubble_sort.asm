4ffcca48b6689edb4a379096a724fd6bbba1c7c3
Ana Luiza Magalhães <anaslmagalhaes@gmail.com>
2021-06-30 21:34:58 -0300

    Modified some details and added comments

--- bubble_sort.asm
+++ bubble_sort.asm
@@ -1,99 +1,100 @@
 .data 
-	mensagem: .asciiz "Insira o tamanho do vetor a ser ordenado: "
+	message: .asciiz "Enter the size of the array: "
 	Array:
-		.align 2 #alinhamento para receber inteiros
-		.space 32 #array que será lido terá espaço predefinido para 8 inteiros.
-	espaco: .asciiz " "
-	inputInvalido: .asciiz "Tamanho invalido!"
+		.align 2 #int alignment
+		.space 32 #array default max size = 8
+	blank_space: .asciiz " "
+	size_error: .asciiz "The entered size is invalid!"
 .text
 	main:
 		li $v0, 4 
-		la $a0, mensagem 
+		la $a0, message 
 		syscall 
 		
 		li $v0, 5 
 		syscall
 		
-		blt $v0, 1, Entrada_Invalida
+		blt $v0, 1, invalid_input
 		
 		
 		move $t0, $v0
 		
-		li $t1, 0 #contador para leitura dos inteiros
-		la $s0, Array #array da memoria
+		la $s0, Array 
+		li $s1, 0 #iterator
 		
-		LerArray:
-			beq $t1, $t0, Bubble_Sort #se $t1 = $t0 entao para a leitura
+		read_array:
+			beq $s1, $t0, bubble_sort #if $s1 = $s0 -> stops input reading
 			
 			li $v0, 5
 			syscall
 			
 			move $t3, $v0
-			sw $t3, ($s0) #salva no array
+			sw $t3, ($s0) #saves to array
 			
-			addi $t1, $t1, 1
-			addi $s0, $s0, 4 #incrementa endereço em 4 bytes
-			j LerArray
+			addi $s1, $s1, 1
+			addi $s0, $s0, 4 #array address pointer increments by 4
+			j read_array
 			
-		Bubble_Sort:	
+		bubble_sort:	
 			subi $t0, $t0, 1 #n-1
-			li $t2, 1 #verifica se houve troca de posicao
+			li $s2, 1 #checks if there was a sorting in the last iteration 
 		
-			While:
-				li $t3, 0 #iterador (i) 
-				beq $t2, 0, Fim #finaliza o programa se nao houver nenhuma troca na iteraçao anterior
-				li $t2, 0 
-				la $s0, Array #reseta endereço do Array para a primeira posição
-				For:
-					beq $t3, $t0, While # i < n-1
-					lw $t4, ($s0) #menor posicao
-					lw $t5, 4($s0) #maior posicao
-					bgt $t4, $t5, Sort #verifica esta desordenado
-					j Iterador
-				Sort: 
-					li $t2, 1 #houve troca de posicao
-					#carrega valores para troca
-					lw $t4, ($s0) 
-					lw $t5, 4($s0) 
-					#troca prosições
-					sw $t4, 4($s0) 
-					sw $t5, ($s0) 
-					j For 
-					
-				Iterador: 
-					addi $s0, $s0, 4 #atualiza endereco
-					addi $t3, $t3, 1 #incrementa contador
-					j For
-			
-			Fim: #print do array
-				la $s0, Array
-				li $t3, 0
+			while:
+				beq $s2, 0, end #(if $s2 = 0, the array is sorted)
+				li $s2, 0 #starts without any swap
+				li $s3, 0 #iterator (i) 
+				la $s0, Array #address pointer goes back to the first index of the array
 				
-				Print:
-					bgt $t3, $t0, Exit 
+				for:
+					beq $s3, $t0, while # i < n-1
+					
+					lw $s4, ($s0) #lowest position
+					lw $s5, 4($s0) #highest position
+					
+					bgt $s4, $s5, sort #verify if it's sorted
+					j iterator
+				sort: 
+					li $s2, 1 #swapped position
+					
+					#sorting#
+					sw $s4, 4($s0) 
+					sw $s5, ($s0) 
+					#########
+					
+					j iterator 
+					
+				iterator: 
+					addi $s0, $s0, 4 #address pointer++
+					addi $s3, $s3, 1 #increments counter
+					j for
+			
+			end: #print
+				la $s0, Array
+				li $s3, 0
+				
+				print:
+					bgt $s3, $t0, exit
 					
 					li $v0, 1
 					lw $a0, ($s0)
 					syscall
 					
 					addi $s0, $s0, 4
-					addi $t3, $t3, 1
+					addi $s3, $s3, 1
 					
 					li $v0, 4
-					la $a0, espaco
+					la $a0, blank_space
 					syscall
 					
-					j Print
-			
-			Exit:
+					j print
+					
+			invalid_input:
+				li, $v0, 4
+				la $a0, size_error
+				syscall	
+
+			exit:
 				li $v0, 10
 				syscall	
-					
-			Entrada_Invalida:
-				li, $v0, 4
-				la $a0, inputInvalido
-				syscall	
-				
-				j Exit	
 			
 		
